2dc7bc6d2dd7efd5a6db41eb335c4fd84909f758
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-09-06 18:19:39 -0300

    Add função limpar e style responsivo

--- script/calendario.js
+++ script/calendario.js
@@ -1,9 +1,20 @@
+//Mostra Calendário Unidade São Paulo
 function mostraSP(){
     document.getElementById('sp').style.display = 'block';
     document.getElementById('santos').style.display = 'none';
 }
 
+//Mostra Calendário Unidade Santos
 function mostraSantos(){
     document.getElementById('sp').style.display = 'none';
     document.getElementById('santos').style.display = 'block';
+}
+
+//Limpa a tela
+function limpar(){
+    document.getElementById('sp').style.display = 'none';
+    document.getElementById('santos').style.display = 'none';
+    const text = "";
+    document.getElementById('nome').innerHTML = text;
+    document.getElementById('email').innerHTML = text;
 }
