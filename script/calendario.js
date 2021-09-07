471a1e825939818a6a1b17939f676787774c88da
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-09-07 20:04:10 -0300

    Teste inserção de Calendário Dinâmico

--- script/calendario.js
+++ script/calendario.js
@@ -1,19 +1,18 @@
 //Mostra Calendário Unidade São Paulo
 function mostraSP(){
     document.getElementById('sp').style.display = 'block';
-    document.getElementById('santos').style.display = 'none';
+    
 }
 
 //Mostra Calendário Unidade Santos
 function mostraSantos(){
-    document.getElementById('sp').style.display = 'none';
-    document.getElementById('santos').style.display = 'block';
+    document.getElementById('sp').style.display = 'block';
+    
 }
 
 //Limpa a tela
 function limpar(){
     document.getElementById('sp').style.display = 'none';
-    document.getElementById('santos').style.display = 'none';
     const text = "";
     document.getElementById('nome').innerHTML = text;
     document.getElementById('email').innerHTML = text;
